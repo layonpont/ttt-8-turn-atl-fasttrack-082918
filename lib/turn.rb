b14b8c4924ef05ec88ee358be09f8aa631d8af84
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-30 17:30:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,13 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
+
 def valid_move(board, index)
-  # taken = nil
-  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-  #   taken = false
-  # else
-  #   taken = true
-  # end
+  
 end 
 
 def move(board)
